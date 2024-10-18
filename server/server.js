d688e74c7429ed94b830de6708f4a9bc7728b79a
VICTOR ALEXANDRE CARVALHO PEDROZA <00001080466721sp@aluno.educacao.sp.gov.br>
2024-10-18 16:27:46 -0300

    Uploado Backend

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2/promise'); // Usando mysql2 com suporte a Promises
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
@@ -17,24 +17,30 @@ app.use(cors({
 }));
 app.use(helmet());
 
-async function connectToDatabase() {
-    const db = mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
-    });
 
+let db;
+
+async function connectToDatabase() {
     try {
-        await db.connect(); // Aguarda a conexão
-        console.log('Conectado com o BD');
+        // Verifica se a conexão já existe e se não está fechada
+        if (!db || db.connection._closing) {
+            db = await mysql.createConnection({
+                host: process.env.DB_HOST || "localhost",  // Usar variáveis de ambiente, se disponíveis
+                user: process.env.DB_USER || "root",
+                password: process.env.DB_PASSWORD || "",
+                database: process.env.DB_NAME || "biblioteca"
+            });
+            console.log('Conectado com o BD');
+        }
         return db; // Retorna a conexão
     } catch (err) {
-        console.error('Erro ao conectar com o BD:', err.message);
-        process.exit(1);
+        console.error('Erro ao conectar com o banco de dados:', err.message);
+        throw new Error('Erro ao conectar com o banco de dados');
     }
 }
 
+
+// Middleware para verificar usuário autenticado
 const verifyUser = (req, res, next) => {
     const token = req.cookies.token;
     if (!token) {
@@ -51,40 +57,40 @@ const verifyUser = (req, res, next) => {
     }
 };
 
-/* const loginLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutos
-    max: 5, // Limite de 5 tentativas
-    message: "Muitas tentativas de login. Tente novamente mais tarde."
-}); */
-
+// Rota principal (com usuário verificado)
 app.get('/', verifyUser, (req, res) => {
     return res.json({ Message: "Success", name: req.name });
 });
 
-app.get('/User', verifyUser, (req, res) => {
-    const sql = 'SELECT * FROM usuario WHERE Nome = ?';
-    db.query(sql, [req.name], (err, data) => {
-        if (err) return res.json({ Message: "Erro ao buscar dados do usuário" });
+// Rota para buscar dados do usuário
+app.get('/User', verifyUser, async (req, res) => {
+    try {
+        const db = await connectToDatabase();
+        const [data] = await db.execute('SELECT * FROM usuario WHERE Nome = ?', [req.name]);
         if (data.length > 0) {
             return res.json({ Status: "Success", user: data[0] });
         } else {
             return res.json({ Message: "Usuário não encontrado" });
         }
-    });
+    } catch (err) {
+        console.error('Erro ao buscar dados do usuário:', err.message);
+        res.status(500).json({ Message: "Erro no servidor" });
+    }
 });
 
-
+// Rota para login
 app.post("/Login", [
     body('email').isEmail(),
     body('password').isLength({ min: 5 })
-], (req, res) => {
+], async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    const sql = 'SELECT * FROM usuario WHERE Email = ? AND Senha = ?';
-    db.query(sql, [req.body.email, req.body.password], (err, data) => {
-        if (err) return res.json({ Message: "Server Side Error" });
+
+    try {
+        const db = await connectToDatabase();
+        const [data] = await db.execute('SELECT * FROM usuario WHERE Email = ? AND Senha = ?', [req.body.email, req.body.password]);
         if (data.length > 0) {
             const name = data[0].Nome;
             const token = jwt.sign({ name }, 'our-jsonwebtoken-secret-key', { expiresIn: "1d" });
@@ -93,60 +99,84 @@ app.post("/Login", [
         } else {
             return res.json({ Message: "Usuário não encontrado" });
         }
-    });
+    } catch (err) {
+        console.error('Erro no login:', err.message);
+        res.status(500).json({ Message: "Erro no servidor" });
+    }
 });
 
+// Rota para logout
 app.get('/Logout', (req, res) => {
     res.clearCookie('token');
     return res.json({ Status: "Success" });
 });
 
-app.get('/livros/:id/disponibilidade', (req, res) => {
+// Verificação de disponibilidade do livro
+app.get('/livros/:id/disponibilidade', async (req, res) => {
     const livroId = req.params.id;
-    const sql = 'SELECT qtd FROM livros WHERE id = ?';
-    db.query(sql, [livroId], (err, data) => {
-      if (err) return res.json({ Message: "Erro ao buscar disponibilidade do livro" });
-      if (data.length > 0 && data[0].qtd > 0) {
-        return res.json({ disponivel: true });
-      } else {
-        return res.json({ disponivel: false });
-      }
-    });
-  });
-  
-
-app.get('/Livros', (req, res) => {
-    const sql = "SELECT * FROM livro;";
-    db.query(sql, (err, data) => {
-        if (err) return res.json(err);
-        return res.json(data);
-    });
+    try {
+        const db = await connectToDatabase();
+        const [data] = await db.execute('SELECT qtd FROM livros WHERE id = ?', [livroId]);
+        if (data.length > 0 && data[0].qtd > 0) {
+            return res.json({ disponivel: true });
+        } else {
+            return res.json({ disponivel: false });
+        }
+    } catch (err) {
+        console.error('Erro ao buscar disponibilidade do livro:', err.message);
+        res.status(500).json({ Message: "Erro no servidor" });
+    }
 });
 
-app.get('/Historia', (req, res) => {
-    const sql = "SELECT * FROM livro WHERE categoria LIKE '%Historia%';";
-    db.query(sql, (err, data) => {
-        if (err) return res.json(err);
+// Rota para buscar todos os livros
+app.get('/Livros', async (req, res) => {
+    try {
+        const db = await connectToDatabase();
+        const [data] = await db.execute("SELECT * FROM livro;");
         return res.json(data);
-    });
+    } catch (err) {
+        console.error('Erro ao buscar livros:', err.message);
+        res.status(500).json({ Message: "Erro no servidor" });
+    }
 });
 
-app.get('/Biografias', (req, res) => {
-    const sql = "SELECT * FROM livro WHERE categoria LIKE '%Biografias%';";
-    db.query(sql, (err, data) => {
-        if (err) return res.json(err);
+// Rota para buscar livros de História
+app.get('/Historia', async (req, res) => {
+    try {
+        const db = await connectToDatabase();
+        const [data] = await db.execute("SELECT * FROM livro WHERE categoria LIKE '%Historia%';");
         return res.json(data);
-    });
+    } catch (err) {
+        console.error('Erro ao buscar livros de História:', err.message);
+        res.status(500).json({ Message: "Erro no servidor" });
+    }
 });
 
-app.get('/Eventos', (req, res) => {
-    const sql = "SELECT * FROM evento;";
-    db.query(sql, (err, data) => {
-        if (err) return res.json({ Message: "Erro ao buscar eventos" });
+// Rota para buscar Biografias
+app.get('/Biografias', async (req, res) => {
+    try {
+        const db = await connectToDatabase();
+        const [data] = await db.execute("SELECT * FROM livro WHERE categoria LIKE '%Biografias%';");
         return res.json(data);
-    });
+    } catch (err) {
+        console.error('Erro ao buscar biografias:', err.message);
+        res.status(500).json({ Message: "Erro no servidor" });
+    }
 });
 
+// Rota para buscar eventos
+app.get('/Eventos', async (req, res) => {
+    try {
+        const db = await connectToDatabase();
+        const [data] = await db.execute("SELECT * FROM evento;");
+        return res.json(data);
+    } catch (err) {
+        console.error('Erro ao buscar eventos:', err.message);
+        res.status(500).json({ Message: "Erro no servidor" });
+    }
+});
+
+// Rota para criar aluguel
 app.post("/aluguel", [
     body('usuario_id').isInt().withMessage('O ID do usuário deve ser um número inteiro.'),
     body('livro_id').isInt().withMessage('O ID do livro deve ser um número inteiro.'),
@@ -154,7 +184,7 @@ app.post("/aluguel", [
     body('data_devolucao').isISO8601().withMessage('A data de devolução deve ser uma data válida.'),
     body('status').isIn(['ativo', 'inativo', 'cancelado']).withMessage('O status deve ser "ativo", "inativo" ou "cancelado".'),
     body('observacoes').optional().isString().withMessage('Observações devem ser uma string.')
-], (req, res) => {
+], async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
@@ -162,74 +192,56 @@ app.post("/aluguel", [
 
     const { usuario_id, livro_id, data_inicio, data_devolucao, status, observacoes } = req.body;
 
-    // Verifica se o usuário já possui um aluguel ativo
-    const checkUserSql = 'SELECT * FROM aluguel WHERE usuario_id = ? AND status = "ativo"';
-    db.query(checkUserSql, [usuario_id], (err, userResults) => {
-        if (err) {
-            return res.status(500).json({ message: "Erro ao verificar aluguéis anteriores" });
-        }
+    try {
+        const db = await connectToDatabase();
 
+        // Verifica se o usuário já possui um aluguel ativo
+        const [userResults] = await db.execute('SELECT * FROM aluguel WHERE usuario_id = ? AND status = "ativo"', [usuario_id]);
         if (userResults.length > 0) {
             return res.status(400).json({ message: "O usuário já possui um livro alugado e ativo." });
         }
 
-        // Contar total de livros disponíveis e quantos estão alugados
-        const countSql = 'SELECT qtd FROM livro WHERE ID = ?';
-        db.query(countSql, [livro_id], (err, livroResults) => {
-            if (err) {
-                return res.status(500).json({ message: "Erro ao verificar a disponibilidade do livro" });
-            }
+        // Contar total de livros disponíveis
+        const [livroResults] = await db.execute('SELECT qtd FROM livro WHERE ID = ?', [livro_id]);
+        if (livroResults.length === 0) {
+            return res.status(404).json({ message: "Livro não encontrado." });
+        }
 
-            if (livroResults.length === 0) {
-                return res.status(404).json({ message: "Livro não encontrado." });
-            }
+        const totalLivros = livroResults[0].qtd;
 
-            const totalLivros = livroResults[0].qtd;
+        // Contar quantos livros estão alugados (status "ativo")
+        const [alugadosResults] = await db.execute('SELECT COUNT(*) AS totalAlugados FROM aluguel WHERE livro_id = ? AND status = "ativo"', [livro_id]);
+        const totalAlugados = alugadosResults[0].totalAlugados;
 
-            // Contar quantos livros estão alugados (status "ativo")
-            const countAlugadosSql = 'SELECT COUNT(*) AS totalAlugados FROM aluguel WHERE livro_id = ? AND status = "ativo"';
-            db.query(countAlugadosSql, [livro_id], (err, alugadosResults) => {
-                if (err) {
-                    return res.status(500).json({ message: "Erro ao contar livros alugados" });
-                }
+        // Calcular a disponibilidade
+        const disponibilidade = totalLivros - totalAlugados;
 
-                const totalAlugados = alugadosResults[0].totalAlugados;
+        if (disponibilidade <= 0) {
+            return res.status(400).json({ message: "Livro não está disponível para aluguel." });
+        }
 
-                // Calcular a disponibilidade
-                const disponibilidade = totalLivros - totalAlugados;
+        // Inserção do novo aluguel
+        const [results] = await db.execute(
+            'INSERT INTO aluguel (usuario_id, livro_id, data_inicio, data_devolucao, status, observacoes) VALUES (?, ?, ?, ?, ?, ?)',
+            [usuario_id, livro_id, data_inicio, data_devolucao, status, observacoes]
+        );
 
-                if (disponibilidade <= 0) {
-                    return res.status(400).json({ message: "Livro não está disponível para aluguel." });
-                }
-
-                // Se não houver aluguel ativo e o livro estiver disponível, prossegue com a inserção do novo aluguel
-                const insertSql = 'INSERT INTO aluguel (usuario_id, livro_id, data_inicio, data_devolucao, status, observacoes) VALUES (?, ?, ?, ?, ?, ?)';
-                db.query(insertSql, [usuario_id, livro_id, data_inicio, data_devolucao, status, observacoes], (err, results) => {
-                    if (err) {
-                        return res.status(500).json({ message: "Erro ao criar aluguel" });
-                    }
-                    res.status(201).json({
-                        id: results.insertId,
-                        usuario_id,
-                        livro_id,
-                        data_inicio,
-                        data_devolucao,
-                        status,
-                        observacoes
-                    });
-                });
-            });
-        });
-    });
+        return res.json({ message: "Aluguel criado com sucesso.", aluguelId: results.insertId });
+    } catch (err) {
+        console.error('Erro ao criar aluguel:', err.message);
+        res.status(500).json({ Message: "Erro no servidor" });
+    }
 });
 
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ Message: "Erro no servidor" });
+// Rate limit para proteger contra abusos
+const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutos
+    max: 100 // Limite de 100 requests por IP
 });
+app.use('/api/', apiLimiter);
 
-app.listen(process.env.PORT, () => {
-    console.log("Rodando na porta: 8088");
+// Porta de escuta do servidor
+const port = process.env.DB_PORT || 3001;
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
 });
-
-module.exports = app; 
